c44f1dbdf0755356c54247737748be38f210f49c
bibersun <bibersun@gmail.com>
2023-06-09 17:24:39 +0300

    initial2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num2 > num1)
+{
+    Console.WriteLine("Второе больше первого");
+}
+else if (num2 < num1)
+{
+    Console.WriteLine("Первое больше второго");
+}
+else
+{
+    Console.WriteLine("Равны");
+}
